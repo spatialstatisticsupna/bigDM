0d1d92f50f4e740ed74a4e37d04789450e400001
Aritz Adin <aritz.adin@unavarra.es>
2024-08-13 10:05:58 +0200

    Fix errors with INLA_24.06.27

--- R/Mmodel_iid.R
+++ R/Mmodel_iid.R
@@ -20,65 +20,65 @@
 #' @param theta Vector of hyperparameters.
 #'
 #' @return This is used internally by the \code{INLA::inla.rgeneric.define()} function.
-#' 
+#'
 #' @import Matrix
-#' 
+#'
 #'
 #' @export
 ########################################################################
 ## Mmodels - idd - FE (BARTLETT DECOMPOSITION)
 ########################################################################
 Mmodel_iid <- function(cmd=c("graph","Q","mu","initial","log.norm.const","log.prior","quit"), theta=NULL){
-  
+
   envir <- parent.env(environment())
   if(!exists("cache.done", envir=envir)){
-    BlockIW <- Diagonal(nrow(W)*J)
-    
+    BlockIW <- Matrix::Diagonal(nrow(W)*J)
+
     assign("BlockIW", BlockIW, envir=envir)
     assign("cache.done", TRUE, envir=envir)
   }
-  
+
   ########################################################################
   ## theta
   ########################################################################
   interpret.theta <- function(){
     diag.N <- sapply(theta[as.integer(1:J)], function(x) { exp(x) })
     no.diag.N <- theta[as.integer(J+1:(J*(J-1)/2))]
-    
-    N <- diag(diag.N,J) 
+
+    N <- diag(diag.N,J)
     N[lower.tri(N, diag=FALSE)] <- no.diag.N
-    
+
     Covar <- N %*% t(N)
-    
+
     e <- eigen(Covar)
     M <- t(e$vectors %*% diag(sqrt(e$values)))
     # S <- svd(Covar)
     # M <- t(S$u %*% diag(sqrt(S$d)))
-    
+
     return(list(Covar=Covar, M=M))
   }
-  
+
   ########################################################################
   ## Graph of precision function; i.e., a 0/1 representation of precision matrix
   ########################################################################
   graph <- function(){ return(Q()) }
-  
+
   ########################################################################
   ## Precision matrix
   ########################################################################
   Q <- function(){
     param <- interpret.theta()
     M.inv <- solve(param$M)
-    MI <- kronecker(M.inv, Diagonal(nrow(W)))
-    Q <- (MI %*% BlockIW) %*% t(MI)
+    MI <- kronecker(M.inv, Matrix::Diagonal(nrow(W)))
+    Q <- (MI %*% BlockIW) %*% Matrix::t(MI)
     return(Q)
   }
-  
+
   ########################################################################
   ## Mean of model
   ########################################################################
   mu <- function(){ return(numeric(0)) }
-  
+
   ########################################################################
   ## log.norm.const
   ########################################################################
@@ -86,19 +86,19 @@ Mmodel_iid <- function(cmd=c("graph","Q","mu","initial","log.norm.const","log.pr
     val <- numeric(0)
     return(val)
   }
-  
+
   ########################################################################
   ## log.prior: return the log-prior for the hyperparameters
   ########################################################################
   log.prior <- function(){
     param <- interpret.theta()
-    
+
     ## n^2_jj ~ chisq(J-j+1) ##
     val <- J*log(2) + 2 *sum(theta[1:J]) + sum(dchisq(exp(2*theta[1:J]), df=(J+2)-1:J+1, log=TRUE))
-    
+
     ## n_ji ~ N(0,1) ##
     val <- val + sum(dnorm(theta[as.integer(J+1:(J*(J-1)/2))], mean=0, sd=1, log=TRUE))
-    
+
     return(val)
   }
 
@@ -106,14 +106,14 @@ Mmodel_iid <- function(cmd=c("graph","Q","mu","initial","log.norm.const","log.pr
   ## initial: return initial values
   ########################################################################
   initial <- function(){ return(as.vector(initial.values)) }
-  
+
   ########################################################################
   ########################################################################
   quit <- function(){ return(invisible()) }
-  
+
   if(!length(theta)) theta <- initial()
   val <- do.call(match.arg(cmd), args=list())
-  
+
   return(val)
 }
 
